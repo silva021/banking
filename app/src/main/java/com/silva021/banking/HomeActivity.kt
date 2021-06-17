13a5c0e821e8e38e778fd31e1d9f55b5af03bfe1
Silva <lucasssilva021@gmail.com>
2021-06-16 22:54:26 -0300

    [FEATURE][NAVIGATION-MODULARIZATION] - Create base navigation and begin of the modularization

--- app/src/main/java/com/silva021/banking/HomeActivity.kt
+++ app/src/main/java/com/silva021/banking/HomeActivity.kt
@@ -1,11 +1,20 @@
 package com.silva021.banking
 
+import android.animation.Animator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.silva021.banking.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
     }
+
 }
